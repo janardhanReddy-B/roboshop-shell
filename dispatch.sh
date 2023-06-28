7d3b45284e2f87680a81b74b60909609f1afeb20
JanardhanReddy <bommireddy246@gmail.com>
2023-06-28 22:17:01 +0100

    errors

--- dispatch.sh
+++ dispatch.sh
@@ -1,28 +1,28 @@
-echo -i -e "\e[33mINSTALLING GOLANG\e[0m"
+echo  -e "\e[33mINSTALLING GOLANG\e[0m"
 yum install golang -y &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mADDING USER\e[0m"
+echo  -e "\e[33mADDING USER\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mGETTING DISPATCH CONTENT\e[0m"
+echo -e "\e[33mGETTING DISPATCH CONTENT\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -i -e "\e[33mUNZIP DISPATCH CONTENT\e[0m"
+echo -e "\e[33mUNZIP DISPATCH CONTENT\e[0m"
 unzip /tmp/dispatch.zip &>>/tmp/roboshop.log
 cd /app &>>/tmp/roboshop.log
 
-echo -i -e "\e[33m INITIALIZE AND AND BUILD DISPATCH\e[0m"
+echo -e "\e[33m INITIALIZE AND AND BUILD DISPATCH\e[0m"
 go mod init dispatch &>>/tmp/roboshop.log
 go get &>>/tmp/roboshop.log
 go build &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mCOPYING GOLANG SERVICE\e[0m"
+echo -e "\e[33mCOPYING GOLANG SERVICE\e[0m"
 cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mSTARTING GOLANG\e[0m"
+echo -e "\e[33mSTARTING GOLANG\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable dispatch &>>/tmp/roboshop.log
 systemctl start dispatch &>>/tmp/roboshop.log
--- payment.sh
+++ payment.sh
@@ -1,26 +1,26 @@
-echo -i -e "\e[33mINSTALLING PYTHON\e[0m"
+echo -e "\e[33mINSTALLING PYTHON\e[0m"
 yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mADDING USER\e[0m"
+echo -e "\e[33mADDING USER\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -i -e "\e[33mADDED PAYMENT CONTENT\e[0m"
+echo -e "\e[33mADDED PAYMENT CONTENT\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -i -e "\e[33mUNZIP PAYMENT CONTENT\e[0m"
+echo -e "\e[33mUNZIP PAYMENT CONTENT\e[0m"
 unzip /tmp/payment.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -i -e "\e[33mINSTALLING PIP3 AND REQUIREMENTS\e[0m"
+echo -e "\e[33mINSTALLING PIP3 AND REQUIREMENTS\e[0m"
 pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mCOPYING PAYMENT SERVICE\e[0m"
+echo -e "\e[33mCOPYING PAYMENT SERVICE\e[0m"
 cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 
-echo -i -e "\e[33mSTARTING PAYMENT\e[0m"
+echo -e "\e[33mSTARTING PAYMENT\e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable payment &>>/tmp/roboshop.log
 systemctl start payment &>>/tmp/roboshop.log
