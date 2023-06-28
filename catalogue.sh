5029c29d34e0ef78e79484945dda7ff3741f3b39
JanardhanReddy <bommireddy246@gmail.com>
2023-06-29 00:40:28 +0100

    errors

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,9 @@
-echo -e "\e[33mCONFIGURING NODEJS\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33mINSTALLING NODEJS\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+source common.sh
+component=catalogue
 
-echo -e "\e[33mADDING ROBOSHOP USER\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[33mREMOVING AND ADDING APP FOLDER\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+nodejs
 
-echo -e "\e[33mADDED CATALOGUE CONTENT\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33mUNZIP CATALOGUE CONTENT\e[0m"
-cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33m INSTALLING NODEJS DEPENDENCIES\e[0m"
-cd /app
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[33mCOPYING CATALOGUE SERVICE\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-
-echo -e "\e[33mSTARTING CATALOGUE SERVICE\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
-
-echo -e "\e[33mCOPYING MONGODB REPO\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
-
-echo -e "\e[33mINSTALLING MONGODB SHELL\e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-
-echo -e "\e[33mRUNNING MONGODB SCHEMA\e[0m"
-mongo --host mongodb-dev.devopsbjr.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo_schema_setup
 
---
+++ common.sh
@@ -0,0 +1,49 @@
+app_path="/app"
+color="\e[33m"
+nocolor="\e[0m"
+log_file="/tmp/roboshop.log"
+
+nodejs() {
+  echo -e "${color} CONFIGURING NODEJS${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+  echo -e ${color}INSTALLING NODEJS${nocolor}"
+  yum install nodejs -y &>>${log_file}
+
+  echo -e ${color}ADDING ROBOSHOP USER${nocolor}"
+  useradd roboshop &>>${log_file}
+
+  echo -e ${color}REMOVING AND ADDING APP FOLDER${nocolor}"
+  rm -rf ${app_path} &>>${log_file}
+  mkdir ${app_path} &>>${log_file}
+
+  echo -e ${color}ADDED ${component} CONTENT${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+
+  echo -e ${color}UNZIP ${component} CONTENT${nocolor}"
+  cd ${app_path}
+  unzip /tmp/${component}.zip &>>${log_file}
+
+  echo -e ${color} INSTALLING NODEJS DEPENDENCIES${nocolor}"
+  cd ${app_path}
+  npm install &>>${log_file}
+
+  echo -e ${color}COPYING ${component} SERVICE${nocolor}"
+  cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  echo -e ${color}STARTING ${component} SERVICE${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable ${component} &>>${log_file}
+  systemctl restart ${component} &>>${log_file}
+}
+
+mongo_schema_setup() {
+  echo -e ${color}COPYING MONGODB REPO${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+  echo -e ${color}INSTALLING MONGODB SHELL${nocolor}"
+  yum install mongodb-org-shell -y &>>${log_file}
+
+  echo -e ${color}RUNNING MONGODB SCHEMA${nocolor}"
+  mongo --host mongodb-dev.devopsbjr.online <${app_path}/schema/${component}.js &>>${log_file}
+}
