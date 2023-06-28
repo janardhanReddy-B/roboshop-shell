a0ace94ded37a2f9e75715b0c9f573c2d5834c93
JanardhanReddy <bommireddy246@gmail.com>
2023-06-29 00:42:04 +0100

    errors

--- cart.sh
+++ cart.sh
@@ -1,32 +1,3 @@
-echo -e "\e[33mCONFIGURING NODEJS\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
-
-echo -e "\e[33mINSTALLING NODEJS\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
-
-echo -e "\e[33mADDING ROBOSHOP USER\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[33mREMOVING AND ADDING APP FOLDER\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
-
-echo -e "\e[33mADDED CART CONTENT\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33mUNZIP CART CONTENT\e[0m"
-cd /app
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33mINSTALLING NODEJS DEPENDENCIES\e[0m"
-cd /app
-npm install &>>/tmp/roboshop.log
-
-echo -e "\e[33mCOPYING CART SERVICE\e[0m"
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
-
-
-echo -e "\e[33mSTARTING CATALOGUE SERVICE\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+source common.sh
+component=cart
+nodejs
